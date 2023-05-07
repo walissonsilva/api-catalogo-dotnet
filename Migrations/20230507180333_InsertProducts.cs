d6bd54da891019cad8e44d309aaf8074b3bae2c5
Walisson Silva <walissonsilva10@gmail.com>
2023-05-07 18:17:55 -0300

    feat: create migration to insert products to database

---
+++ Migrations/20230507180333_InsertProducts.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "INSERT INTO Products(Name,Description,Price,ImageUrl,Inventory,RegistrationDate,categoryId) VALUES('Coca Cola Zero', 'Refrigerante de Cola 350 ml', 5.45, 'coca-cola.jpg', 50, now(), 1)"
+            );
+            migrationBuilder.Sql(
+                "INSERT INTO Products(Name,Description,Price,ImageUrl,Inventory,RegistrationDate,categoryId) VALUES('Lanche de Atum', 'Lanche de Atum com Maionese', 8.50, 'atum.jpg', 10, now(), 2)"
+            );
+            migrationBuilder.Sql(
+                "INSERT INTO Products(Name,Description,Price,ImageUrl,Inventory,RegistrationDate,categoryId) VALUES('Pudim 100g', 'Pudim de Leite Condensado 100g', 6.75, 'pudim.jpg', 20, now(), 3)"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Products");
+        }
+    }
+}
