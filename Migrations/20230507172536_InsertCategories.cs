0168332d9750304044052893188257a9da518757
Walisson Silva <walissonsilva10@gmail.com>
2023-05-07 15:02:58 -0300

    feat: create migration to insert categories to database

---
+++ Migrations/20230507172536_InsertCategories.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "INSERT INTO categories(name, imageUrl) VALUES('Bebida', 'bebidas.jpg')"
+            );
+            migrationBuilder.Sql(
+                "INSERT INTO categories(name, imageUrl) VALUES('Lanches', 'lanches.jpg')"
+            );
+            migrationBuilder.Sql(
+                "INSERT INTO categories(name, imageUrl) VALUES('Sobremesas', 'sobremesas.jpg')"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM categories");
+        }
+    }
+}
