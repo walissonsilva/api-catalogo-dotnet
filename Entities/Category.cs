154d395ad5906150537edbd17a456ecc3f570dca
Walisson Silva <walissonsilva10@gmail.com>
2023-05-07 14:25:02 -0300

    feat: add data annotations on entities

--- Entities/Category.cs
+++ Entities/Category.cs
@@ -1,7 +1,10 @@
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Entities;
 
+[Table("Categories")]
 public class Category
 {
     public Category()
@@ -9,8 +12,15 @@ public class Category
         Products = new Collection<Product>();
     }
 
+    [Key]
     public int CategoryId { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public string? Name { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? ImageUrl { get; set; }
 
     public ICollection<Product>? Products { get; set; }
--- Entities/Product.cs
+++ Entities/Product.cs
@@ -1,11 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace APICatalogo.Entities;
 
+[Table("Products")]
 public class Product
 {
+    [Key]
     public int ProductId { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public string? Name { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? Description { get; set; }
+
+    [Required]
+    [Column(TypeName = "decimal(10,2)")]
     public decimal Price { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? ImageUrl { get; set; }
     public float Inventory { get; set; }
     public DateTime RegistrationDate { get; set; }
